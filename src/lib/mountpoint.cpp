e22269db550675a757867527219d69bf3352de55
Shane <ssteidley@gnsoftwarelabs.com>
2018-05-16 09:56:04 -0500

    Implemented nullfs mount.  Still needs to be tested. Added appc_mount but has not yet been implemented.

--- src/lib/mountpoint.cpp
+++ src/lib/mountpoint.cpp
@@ -1,6 +1,107 @@
 #include "mountpoint.h"
 
-mountpoint::mountpoint()
-{
+#include "fs.h"
+#include <iostream>
+#include <map>
+#include <string>
 
+#include <sys/param.h>
+#include <sys/mount.h>
+
+#include <sys/uio.h>
+#include <vector>
+
+using namespace appc;
+
+namespace
+{
+    class base_mountpoint : public mountpoint
+    {
+        fs_path m_from;
+        fs_path m_target;
+
+        using options = std::map<std::string, std::string>;
+        options m_opts;
+
+        int m_flags;
+
+        std::string m_last_mount_err;
+
+    protected:
+
+        void add_option(const std::string& option, const std::string& value)
+        {
+            m_opts[option] = value;
+        }
+
+    public:
+
+        std::string fspath() {
+
+            return m_opts["fspath"];
+        }
+
+        base_mountpoint(const fs_path& from, const fs_path& target,
+                        const std::string fs_type, int flags)
+            : m_from(from),
+              m_target(target),
+              m_opts({{"from", m_from.str()},
+                      {"fspath", m_target.str()},
+                      {"fstype", fs_type}}),
+              m_flags(flags),
+              m_last_mount_err()
+        {}
+
+        int mount() override
+        {
+            char errmsg[255];
+            std::vector<struct iovec> iovs;
+
+            struct iovec iov;
+            for(auto& option : m_opts) {
+
+                iov.iov_base = (void*)option.first.c_str();
+                iov.iov_len = option.first.length() + 1;
+                iovs.push_back(iov);
+
+                iov.iov_base = (void*)option.second.c_str();
+                iov.iov_len = option.second.length() + 1;
+                iovs.push_back(iov);
+
+                std::cerr << option.first << "=" << option.second << std::endl;
+            }
+
+            iov.iov_base = (void*)"errmsg";
+            iov.iov_len = sizeof("errmsg");
+            iovs.push_back(iov);
+
+            iov.iov_base = errmsg;
+            iov.iov_len = sizeof(errmsg);
+            iovs.push_back(iov);
+
+            int err = ::nmount(iovs.data(), iovs.size(), m_flags);
+            if(err == -1) {
+                m_last_mount_err = errmsg;
+            }
+
+            return err;
+        }
+
+        int unmount(int flags) override
+        {
+            return ::unmount(m_target.str().c_str(), flags);
+        }
+
+        std::string error_message() override
+        {
+            return m_last_mount_err;
+        }
+    };
+}
+
+std::unique_ptr<mountpoint> appc::create_nullfs_mount(fs_path from,
+                                                      fs_path target,
+                                                      int flags)
+{
+    return std::make_unique<base_mountpoint>(from, target, "nullfs", flags);
 }
--- src/lib/mountpoint.h
+++ src/lib/mountpoint.h
@@ -1,11 +1,29 @@
 #ifndef MOUNTPOINT_H
 #define MOUNTPOINT_H
 
+#include "fs.h"
+#include <memory>
+#include <string>
 
-class mountpoint
+namespace appc
 {
-public:
-    mountpoint();
-};
+    class mountpoint
+    {
+    public:
 
+        virtual int mount() = 0;
+        virtual int unmount(int flags) = 0;
+
+        /**
+         * Error message string from the last mount attempt.
+         * this may be different then the error message associated
+         * with errno
+         */
+        virtual std::string error_message() = 0;
+    };
+
+    std::unique_ptr<mountpoint> create_nullfs_mount(fs_path from,
+                                                    fs_path target,
+                                                    int mount_flags);
+}
 #endif // MOUNTPOINT_H
