ba1bbcf8313fea4d805250353575224da03a98dc
Shane <ssteidley@gnsoftwarelabs.com>
2018-05-16 20:14:36 -0500

    * Auto unmount bind directory. * Comment out jail stuff in appc_mount.

--- src/util/appc_mount.cpp
+++ src/util/appc_mount.cpp
@@ -11,6 +11,21 @@ extern "C"
 }
 using namespace appc;
 
+namespace
+{
+    void auto_unmount(mountpoint* mnt)
+    {
+        std::cerr << "Unmounting..." << std::endl;
+        int err = mnt->unmount(MNT_WAIT);
+        if(err)
+        {
+            std::cerr << "Error unmounting: " << strerror(errno) << std::endl;
+        }
+    }
+
+    using auto_unmount_ptr = std::unique_ptr<mountpoint, decltype(&auto_unmount)>;
+}
+
 int run_main(int argc, char** argv)
 {
     Tcl_FindExecutable(argv[0]);
@@ -44,8 +59,8 @@ int run_main(int argc, char** argv)
         throw std::runtime_error("NYI: Create container directory if it doesn't exist");
     }
 
-    std::unique_ptr<mountpoint> mntpoint = create_nullfs_mount(image_path, container_dir,
-                                                               0);
+    auto_unmount_ptr mntpoint(create_nullfs_mount(image_path, container_dir, 0).release(),
+                              auto_unmount);
 
     std::cerr << "Mounting" << std::endl;
     int error = mntpoint->mount();
@@ -59,25 +74,30 @@ int run_main(int argc, char** argv)
     /*This belongs in a jail object that is destroyed when it goes out
      * of scope
      */
-    struct jail j;
-    memset(&j, 0, sizeof(j));
-    j.hostname = (char*)"ShaneJail";
-    j.jailname = (char*)cmdline->container.c_str();
-    j.version = JAIL_API_VERSION;
-    j.path = (char*)container_dir.str().c_str();
+//    struct jail j;
+//    memset(&j, 0, sizeof(j));
+//    j.hostname = (char*)"ShaneJail";
+//    j.jailname = (char*)cmdline->container.c_str();
+//    j.version = JAIL_API_VERSION;
+//    j.path = (char*)container_dir.str().c_str();
 
-    int jail_id = ::jail(&j);
+//    int jail_id = ::jail(&j);
 
-    if(jail_id != -1)
-    {
-        char hostname[_POSIX_HOST_NAME_MAX];
-        gethostname(hostname, sizeof(hostname));
-        std::cerr << "I'm in a jail!! " << jail_id << " Hostname: " << hostname << std::endl;
-    }
-    else
-    {
-        std::cerr << "Jail error: " << strerror(errno) << std::endl;
-    }
+//    if(jail_id != -1)
+//    {
+//        char hostname[_POSIX_HOST_NAME_MAX];
+//        gethostname(hostname, sizeof(hostname));
+//        std::cerr << "I'm in a jail!! " << jail_id << " Hostname: " << hostname << std::endl;
+//        error = jail_remove(jail_id);
+//        if(error)
+//        {
+//            std::cerr << "Error removing jail: " << strerror(errno) << std::endl;
+//        }
+//    }
+//    else
+//    {
+//        std::cerr << "Jail error: " << strerror(errno) << std::endl;
+//    }
     return 0;
 }
 
