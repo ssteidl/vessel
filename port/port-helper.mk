8220ac6bf1d04c6d65dd909a76cf65eb8842c6f7
Shane Steidley <ssteidley@gnsoftwarelabs.com>
2021-11-14 00:09:20 -0600

    Fixed up the port helper a bit to remove old FBSDAppContainers stuff

--- port/port-helper.mk
+++ port/port-helper.mk
@@ -1,14 +1,14 @@
 # - Run from top level directory
 # - Set DISTDIR=/tmp in environment
 COMMIT!= git rev-parse HEAD
-VERSION?=1.1.2-${COMMIT}
+VERSION?=1.2.0-${COMMIT}
 DISTDIR?=/tmp
 STAGEDIR?=FBSD
 
 distdir: .PHONY
 	mkdir -p ${DISTDIR}
 
-#ssteidl-FBSDAppContainers-1.0-894fa8fe51401e3c8e10bc67fe86a0e21a63e2ae_GH0.tar.gz
+#ssteidl-vessel-1.0-894fa8fe51401e3c8e10bc67fe86a0e21a63e2ae_GH0.tar.gz
 tarball: distdir 
-	git archive -v --format tgz --prefix FBSDAppContainers-${COMMIT}/ -o ${DISTDIR}/ssteidl-FBSDAppContainers-${VERSION}_GH0.tar.gz ${COMMIT}
+	git archive -v --format tgz --prefix vessel-${COMMIT}/ -o ${DISTDIR}/ssteidl-vessel-${VERSION}_GH0.tar.gz ${COMMIT}
 	make -C port makesum
