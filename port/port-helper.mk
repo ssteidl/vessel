d19c67929f129904bb11e1a6bc328492862b61ad
Shane Steidley <steidley.shane@gmail.com>
2022-01-17 16:11:52 -0600

    Support overriding the package version to build head.

--- port/port-helper.mk
+++ port/port-helper.mk
@@ -1,7 +1,8 @@
 # - Run from top level directory
 # - Set DISTDIR=/tmp in environment
-COMMIT!= git rev-parse HEAD
-VERSION?=1.2.1-${COMMIT}
+# - Set _V=`git rev-parse HEAD`
+
+VERSION?=${_V}
 DISTDIR?=/tmp
 STAGEDIR?=FBSD
 
@@ -10,5 +11,5 @@ distdir: .PHONY
 
 #ssteidl-vessel-1.0-894fa8fe51401e3c8e10bc67fe86a0e21a63e2ae_GH0.tar.gz
 tarball: distdir 
-	git archive -v --format tgz --prefix vessel-${COMMIT}/ -o ${DISTDIR}/ssteidl-vessel-${VERSION}_GH0.tar.gz ${COMMIT}
+	git archive -v --format tgz --prefix vessel-${VERSION}/ -o ${DISTDIR}/ssteidl-vessel-${VERSION}_GH0.tar.gz ${VERSION}
 	make -C port makesum
