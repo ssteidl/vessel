44de32fa5e87a85dadc2c92d74959c6a5823a070
Shane Steidley <steidley.shane@gmail.com>
2022-12-28 21:42:28 -0600

    Added all.tcl and test_constraints package

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 #Setup the development environment by sourcing this script
 
-export TCLLIBPATH=$PWD/src/lib/tcl
+export TCLLIBPATH="$PWD/src/lib/tcl $PWD/test"
 export LD_LIBRARY_PATH=$PWD/build
 export PATH=$PWD/src/apps:$PATH
 ln -sf $PWD/build/libvesseltcl.so $PWD/src/lib/tcl/libvesseltcl.so 
