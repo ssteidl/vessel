9884979fab6b89b4a775f3ca28d2fbb5defb1293
Shane <ssgriffonuser@gmail.com>
2023-04-30 21:28:08 -0500

    Added an sqlite amalgamation with sqlite tcl extension.
    I wanted to own the sqlite version so that I could use the latest
    features but remain backwards compatible with older versions of
    freebsd.  For instance, FreeBSD 12.* packages don't support json
    extension.

--- dev.sh
+++ dev.sh
@@ -3,6 +3,7 @@
 export TCLLIBPATH="$PWD/src/lib/tcl $PWD/test"
 export LD_LIBRARY_PATH=$PWD/build
 export PATH=$PWD/src/apps:$PATH
-ln -sf $PWD/build/libvesseltcl.so $PWD/src/lib/tcl/libvesseltcl.so 
+ln -sf $PWD/build/libvesseltcl.so $PWD/src/lib/tcl/libvesseltcl.so
+ln -sf $PWD/build/libvesselsqlite.so $PWD/src/lib/tcl/libvesselsqlite.so 
 
 export EDITOR=emacs
